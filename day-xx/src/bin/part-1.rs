2fb76a70cf4fbe25baa84e57602381fc020e53fa
Shadow <william.shadow@outlook.com>
2023-12-02 19:27:59 +0000

    update template

--- day-xx/src/bin/part-1.rs
+++ day-xx/src/bin/part-1.rs
@@ -3,5 +3,5 @@ use std::fs;
 
 fn main() {
     let file = fs::read_to_string("./input.txt").unwrap();
-    println!("{}", process_part1(file));
+    println!("{}", process_part1(&file));
 }
--- day-xx/src/lib.rs
+++ day-xx/src/lib.rs
@@ -1,8 +1,8 @@
-pub fn process_part1(input: String) -> String {
-    return input.to_string()
+pub fn process_part1(input: &String) -> u32 {
+    return input.parse::<u32>().unwrap();
 }
-pub fn process_part2(input: String) -> String {
-    return input.to_string()
+pub fn process_part2(input: &String) -> u32 {
+    return input.parse::<u32>().unwrap();
 }
 
 #[cfg(test)]
@@ -13,11 +13,11 @@ mod tests {
     #[test]
     fn it_works() {
         let example_input = fs::read_to_string("./example.txt").unwrap();
-        
-        let part1_result = process_part1(example_input.clone());
-        let part2_result = process_part2(example_input);
-        
-        assert_eq!(part1_result, "");
-        assert_eq!(part2_result, "");
+
+        let part1_result = process_part1(&example_input.clone());
+        let part2_result = process_part2(&example_input);
+
+        assert_eq!(part1_result, 0);
+        assert_eq!(part2_result, 0);
     }
 }
